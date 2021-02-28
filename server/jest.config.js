9137622f02adba33e8f1ea89c01770bc45f2be43
Krzysztof <krysbaran@gmail.com>
2021-03-01 00:54:20 +0100

    Ignore tests in dist dir

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch : [ "**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)" ],
   coveragePathIgnorePatterns: [
     "/node_modules/"
   ]
