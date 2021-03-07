c662a2ac41b127e5f42dd405a0ac5dca8910e3c8
Krzysztof <krysbaran@gmail.com>
2021-03-07 20:53:23 +0100

    Set up building in ci

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT : Number = Number(process.env.PORT) || 3000;
+const PORT: Number = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 
@@ -10,11 +10,11 @@ app.listen(PORT, () => {
   console.log(`Server: http://localhost:${PORT}`);
 });
 
-const router = express.Router();
-
 app.get("/", (_, res) => {
-  res.json({"msg":"Works 🐳!"});
+  res.json({ "msg": "Works 🐳!" });
 });
 
+
+const router = express.Router();
 export const serverRoutes = router;
 export const server = app;
